0f674205187429e32d6270879c417f3f1dfd261b
ShikharVeer10 <shikharveer01@gmail.com>
2025-11-29 12:34:26 +0530

    Add Karatsuba.java

---
+++ STS-FAT/Karatsuba.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Karatsuba
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        BigInteger a=sc.nextBigInteger();
+        BigInteger b=sc.nextBigInteger();
+        System.out.println(a.multiply(b));
+    }
+}
